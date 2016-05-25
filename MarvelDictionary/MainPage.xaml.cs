1cc9eafd194c32698d6edcb0670a970887decac8
elykc <elykc@DESKTOP-JJG7VSA>
2016-05-25 10:26:06 -0500

    Agregar archivos de proyecto.

---
+++ MarvelDictionary/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using MarvelDictionary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MarvelDictionary
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<Character> MarvelCharacters { get; set; }
+        public ObservableCollection<ComicBook> MarvelComics { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MarvelCharacters = new ObservableCollection<Character>();
+            MarvelComics = new ObservableCollection<ComicBook>();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyProgressRing.IsActive = true;
+            MyProgressRing.Visibility = Visibility.Visible;
+            while (MarvelCharacters.Count < 10)
+            {
+                Task t = MarvelFacade.PopulateMarvelCharactersAsync(MarvelCharacters);
+                await t;
+            }
+            MyProgressRing.IsActive = false;
+            MyProgressRing.Visibility = Visibility.Collapsed;
+        }
+
+        private async void MasterListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            MyProgressRing.IsActive = true;
+            MyProgressRing.Visibility = Visibility.Visible;
+
+            ComicDetailDescriptionTextBlock.Text = string.Empty;
+            ComicDetailNameTextBlock.Text = string.Empty;
+            ComicDetailImage.Source = null;
+
+            var selectedCharacter = (Character)e.ClickedItem;
+            DetailNameTextBlock.Text = selectedCharacter.name;
+            DetailDescriptionTextBlock.Text = selectedCharacter.description;
+
+            var largeImage = new BitmapImage();
+            Uri uri = new Uri(selectedCharacter.thumbnail.large, UriKind.Absolute);
+            largeImage.UriSource = uri;
+            DetailImage.Source = largeImage;
+
+            MarvelComics.Clear();
+            await MarvelFacade.PopulateMarvelComicsAsync(selectedCharacter.id, MarvelComics);
+
+            MyProgressRing.IsActive = false;
+            MyProgressRing.Visibility = Visibility.Collapsed;
+        }
+
+        private void ComicsGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedComic = (ComicBook)e.ClickedItem;
+
+            ComicDetailNameTextBlock.Text = selectedComic.title;
+
+            if (selectedComic.description != null)
+                ComicDetailDescriptionTextBlock.Text = selectedComic.description;
+            
+
+            var largeImage = new BitmapImage();
+            Uri uri = new Uri(selectedComic.thumbnail.large, UriKind.Absolute);
+            largeImage.UriSource = uri;
+            ComicDetailImage.Source = largeImage;
+
+        }
+    }
+}
